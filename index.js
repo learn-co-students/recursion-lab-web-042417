3966870f84eadd2e844173a05f25942f466d6e3b
kylewelt <kylewelt@umich.edu>
2017-08-14 14:06:50 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,61 @@
 // Code your solution here!
+function printString (myString) {
+  console.log(myString[0])
+  if (myString.length > 1) {
+    let mySubstring = myString.substring(1, myString.length)
+    printString(mySubstring)
+  } else {
+    return true
+  }
+}
 
+function reverseString (myString) {
+  if (myString.length === 0) {
+    return ''
+  } else {
+    return myString[myString.length - 1] +
+      reverseString(myString.substring(0, myString.length - 1))
+  }
+}
+
+function isPalindrome (myString) {
+  if (myString.length === 1) {
+    return true
+  } else if (myString[0] !== myString[myString.length - 1]) {
+    return false
+  } else {
+    return isPalindrome(myString.substring(1, myString.length - 1))
+  }
+}
+
+function addUpTo (members, index) {
+  if (index === 0) {
+    return members[0]
+  } else {
+    return members[0] + addUpTo(members.slice(1, members.length), index - 1)
+  }
+}
+
+function maxOf (members) {
+  if (members.length === 1) {
+    return members[0]
+  } else {
+    if (members[0] > members[members.length - 1]) {
+      return maxOf(members.slice(0, members.length - 1))
+    } else {
+      return maxOf(members.slice(1, members.length))
+    }
+  }
+}
+
+function includesNumber (members, number) {
+  if (members[0] === number) {
+    return true
+  } else {
+    if (members.length === 1) {
+      return false
+    } else {
+      return includesNumber(members.slice(1, members.length), number)
+    }
+  }
+}
